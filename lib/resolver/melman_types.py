dc8de5e9f1b0f60a9b9fbdb756e288dc43980d65
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2023-12-09 16:12:15 +0200

    Fixed recursive import

--- lib/resolver/melman_types.py
+++ lib/resolver/melman_types.py
@@ -1,15 +1,18 @@
+import typing
 from typing import Union, Pattern, Callable, Coroutine, Any, Dict
 
 from telegram.ext import Application, CallbackContext, JobQueue, ExtBot
 
-from lib import MelmanUpdate
+if typing.TYPE_CHECKING:
+    # noinspection PyUnresolvedReferences
+    from lib import MelmanUpdate
 
 MelmanHandlerContext = CallbackContext[ExtBot[None],
                                        Dict[Any, Any],
                                        Dict[Any, Any],
                                        Dict[Any, Any]]
 MelmanRoutes = Union[str, Pattern]
-MelmanCallback = Callable[[MelmanUpdate, MelmanHandlerContext], Coroutine[Any, Any, None]]
+MelmanCallback = Callable[["MelmanUpdate", MelmanHandlerContext], Coroutine[Any, Any, None]]
 MelmanDecoratorWrapper = Callable[[MelmanCallback], Any]
 MelmanApp = Application[ExtBot[None],
                         MelmanHandlerContext,
