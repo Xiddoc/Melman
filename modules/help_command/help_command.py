da8afb1c1673478b077251de73fc16c1fd5b27c1
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2023-12-09 20:07:31 +0200

    Added basic help menu

---
+++ modules/help_command/help_command.py
@@ -0,0 +1,14 @@
+from telegram.ext import ContextTypes
+
+from lib import MelmanModule, MelmanUpdate, MelmanTextHelp
+
+hello_world = MelmanModule("helloworld", help_msg=MelmanTextHelp("Prints a basic hello world message to the screen!"))
+
+
+# noinspection PyUnusedFunction
+@hello_world.route()
+async def index(update: MelmanUpdate, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if not update.message:
+        return
+
+    await update.message.reply_text("Hey, I'm Melman, your favorite giraffe Telegram bot!")
